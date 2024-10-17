f84bf841a7bff7d7ab1178e0097f7d25ce4e257a
Student <tierney@rcc.team>
2024-10-17 00:13:39 -0400

    Added structure for main and added console class

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HomeScreen();
+    }
+
+    /* First Level Menu, exits to end program.
+     */
+    static void HomeScreen() {
+        System.out.println();
+        System.out.println("----------Home Screen-----------");
+        System.out.println("Please select from the following:");
+        System.out.println("(D) Add Deposit");
+        System.out.println("(P) Make Payment");
+        System.out.println("(L) Ledger");
+        System.out.println("(X) Exit");
+        System.out.print("Command: ");
+    }
+
+    static void displayLedgerScreen() {
+
+    }
+
+    static void displayReportScreen() {
+
+    }
+
+    static void displayDeposits() {
+
+    }
+    static void displayPaymentScreen() {
+
+    }
+
+    static void displayAll() {
+    }
+
+    static void doSearch(){
+    System.out.println("doSearch")
+    }
+
+    public static void getUserDepositInformation(){
+
+
+
+       }
+    }
